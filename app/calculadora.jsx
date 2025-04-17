74a9a4a902031809f73d2fe451a76ed60f7cde8d
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2025-04-17 15:34:53 -0300

    feat: alteração das cores

--- app/calculadora.jsx
+++ app/calculadora.jsx
@@ -88,7 +88,7 @@ const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', padding: 20, backgroundColor: '#f0f0f0' },
   titulo: { fontSize: 28, textAlign: 'center', marginBottom: 20, fontWeight: 'bold' },
   input: {
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     borderRadius: 8,
     padding: 12,
     marginBottom: 10,
