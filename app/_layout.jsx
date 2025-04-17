5201ecae07d3570e4c990ba86c0bff2482504c9b
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-04-17 15:33:51 -0300

    feat: criação de icone de navegação

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,42 +3,52 @@ import IonIcons from "react-native-vector-icons/Ionicons";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Layout() {
-return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-        <Drawer>
-            <Drawer.Screen
-                name="index"
-                options={{
-                    drawerLabel: "Início",
-                    title: "Home",
-                    headerShown: false,
-                    headerLeft: true,
-                    drawerIcon: ({ size, color }) => (
-                        <IonIcons name="home" size={size} color={color} />
-                    ),
-                }}
-            />
-            <Drawer.Screen
-                name="contador"
-                options={{
-                    drawerLabel: "Perfil",
-                    title: "Perfil",
-                    drawerIcon: ({ size, color }) => (
-                        <IonIcons name="person" size={size} color={color} />
-                    ),
-                }}
-            />
-            <Drawer.Screen
-                name="calculadora"
-                options={{
-                    drawerLabel: "calculadora",
-                    title: "calculadora",
-                    drawerIcon: ({ size, color }) => (
-                        <IonIcons name="calculator" size={size} color={color} />
-                    ),
-                }}
-            />
-        </Drawer>
-    </GestureHandlerRootView>
-);
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Drawer>
+                <Drawer.Screen
+                    name="index"
+                    options={{
+                        drawerLabel: "Início",
+                        title: "Home",
+                        headerShown: false,
+                        headerLeft: true,
+                        drawerIcon: ({ size, color }) => (
+                            <IonIcons name="home" size={size} color={color} />
+                        ),
+                    }}
+                />
+                <Drawer.Screen
+                    name="contador"
+                    options={{
+                        drawerLabel: "Perfil",
+                        title: "Perfil",
+                        drawerIcon: ({ size, color }) => (
+                            <IonIcons name="person" size={size} color={color} />
+                        ),
+                    }}
+                />
+                <Drawer.Screen
+                    name="calculadora"
+                    options={{
+                        drawerLabel: "calculadora",
+                        title: "calculadora",
+                        drawerIcon: ({ size, color }) => (
+                            <IonIcons name="calculator" size={size} color={color} />
+                        ),
+                    }}
+                />
+                <Drawer.Screen
+                    name="frases"
+                    options={{
+                        drawerLabel: "Frases",
+                        title: "Frases",
+                        drawerIcon: ({ size, color }) => (
+                            <IonIcons name="today" size={size} color={color} />
+                        ),
+                    }}
+                />
+            </Drawer>
+        </GestureHandlerRootView>
+    );
 }
